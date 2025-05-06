e9ce70c7884e7e4c84532ae3556f8e05538d71af
ZeroaF77 <158405501+XzeroaF77@users.noreply.github.com>
2025-05-06 20:59:10 +0800

    Create ft_print_number.c

---
+++ beginner_level/ft_print_number.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_number(int x)
+{
+	int	x;
+	x = '0';
+	while (x <= '9')
+		{
+			ft_putchar(x);
+			x++;
+		}
+}
