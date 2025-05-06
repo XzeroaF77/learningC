815126df13342ecbc06524950b058d43e2632599
ZeroaF77 <158405501+XzeroaF77@users.noreply.github.com>
2025-05-06 17:53:28 +0800

    Update ft_putchar.c

--- ft_putchar.c
+++ ft_putchar.c
@@ -1,11 +1,11 @@
-#include <unistd.h>
+#include <unistd.h>//include header file
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-int	main(void)
+int	main(void)// output 
 {
   ft_putchar('c');
 }
