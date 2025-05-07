ee3542f88d546b505a924d0df17f24f8f04b189a
ZeroaF77 <158405501+XzeroaF77@users.noreply.github.com>
2025-05-07 11:40:21 +0800

    Update ft_putchar.c

--- beginner_level/ft_putchar.c
+++ beginner_level/ft_putchar.c
@@ -1,6 +1,23 @@
-#include <unistd.h>
+#include <unistd.h> //print one character
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
+
+int	main(void)
+{
+	ft_putchar('c');
+}
+
+/*
+#include <stdio.h>//use ASCII values to display certain characters.
+
+int	main(void)
+{
+	char a = 65, b = 66, c = 67;
+	printf("%c", a);
+	printf("%c", b);
+	printf("%c, c);
+}
+*/
