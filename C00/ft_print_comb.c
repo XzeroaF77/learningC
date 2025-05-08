31cac8a8f8e6d85e23483b2a4a670d1c2898b103
ZeroaF77 <158405501+XzeroaF77@users.noreply.github.com>
2025-05-08 16:31:44 +0800

    Update ft_print_comb.c

--- C00/ft_print_comb.c
+++ C00/ft_print_comb.c
@@ -1,1 +1,53 @@
-Print all possible combinations of three different digiys in ascending order, without repetitions and with specific formatting.
+//Print all possible combinations of three different digiys in ascending order, without repetitions and with specific formatting.
+
+#include <unistd.h>
+
+void	ft_print_int(int n)
+{
+	char	*alphabet;
+
+	alphabet = "0123456789";
+	write (1, alphabet + n, 1);
+}
+
+void	ft_print_nums(int a, int b, int c)
+{
+	ft_print_int(a);
+	ft_print_int(b);
+	ft_print_int(c);
+}
+
+void	ft_print_comb(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = 0;
+	while (a <= 7)
+	{
+		b = a + 1;
+		while (b <= 8)
+		{
+			c = b + 1;
+			while (c <= 9)
+			{
+				ft_print_nums(a, b, c);
+				if (a != 7 || b != 8 || c != 9)
+				{
+					write(1, ", ", 2);
+				}
+				c += 1;
+			}
+			b += 1;
+		}
+		a += 1;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}
+*/
