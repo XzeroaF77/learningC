c7eb61e07c3dbe322e00aed9249a22762f820371
ZeroaF77 <158405501+XzeroaF77@users.noreply.github.com>
2025-05-12 23:45:52 +0800

    Update ft_is_negative.c

--- beginner_level/ft_is_negative.c
+++ beginner_level/ft_is_negative.c
@@ -4,15 +4,7 @@
 //Function to print 'N' if the number is negative and print 'P' for positive 
 void    ft_is_negative(int n)//Takes an integer parameter
 {
-        char    negative;//character to print for negative numbers
-        char    positive;//character to print for non-negative numbers
-
-        negative = 'N';
-        positive = 'P';
-        if (n < 0)//check if number is negative
-                write(1, &negative, 1);//Print 'N'
-        else//Number is zero or positive
-                write(1, &positive, 1);//Print 'P'
+        write(1, (n < 0) ? "N" : "P", 1) //ternary operator to simplify
 }
 /*
 int     main(void)
