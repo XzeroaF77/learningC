9ab43a8e875ebf0ecb4d8d6980542b063639b4e8
ZeroaF77 <158405501+XzeroaF77@users.noreply.github.com>
2025-05-12 22:16:11 +0800

    Update ft_is_negative.c

--- beginner_level/ft_is_negative.c
+++ beginner_level/ft_is_negative.c
@@ -1,18 +1,18 @@
 //To check if number is negative or not
 
-#include <unistd.h>
-
-void    ft_is_negative(int n)
+#include <unistd.h>//header file to write()
+//Function to print 'N' if the number is negative and print 'P' for positive 
+void    ft_is_negative(int n)//Takes an integer parameter
 {
-        char    negative;
-        char    positive;
+        char    negative;//character to print for negative numbers
+        char    positive;//character to print for non-negative numbers
 
         negative = 'N';
         positive = 'P';
-        if (n < 0)
-                write(1, &negative, 1);
-        else
-                write(1, &positive, 1);
+        if (n < 0)//check if number is negative
+                write(1, &negative, 1);//Print 'N'
+        else//Number is zero or positive
+                write(1, &positive, 1);//Print 'P'
 }
 /*
 int     main(void)
