54762bec5757bb02bd773fc910795ca9a0d4b8fc
ZeroaF77 <158405501+XzeroaF77@users.noreply.github.com>
2025-05-12 21:15:44 +0800

    Update ft_print_alphabet.c

--- beginner_level/ft_print_alphabet.c
+++ beginner_level/ft_print_alphabet.c
@@ -1,14 +1,14 @@
 //To print a to z in program
 
-#include <unistd.h>
-
-void	ft_print_alphabet(char c)
+#include <unistd.h>//header file for write()
+//Function definition(also serves as declaration in C)
+void	ft_print_alphabet(char c)//should take no arguments since it generates 'a' to 'z' internally
 {
-	char	c;
-	c = 'a';
-	while (c <= 'z')
+	char	c;//declare a character variable to store current letter
+	c = 'a';//initialize c with 'a'
+	while (c <= 'z')//loop from 'a' to 'z'
 	{
-		write(1, &c, 1);
-		c++;
+		write(1, &c, 1);//Write the current character to standard output
+		c++;//increment to the next character(post-increment)
 	}
 }
