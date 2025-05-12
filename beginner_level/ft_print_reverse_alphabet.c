2801d58f1e0a18da8f3b5df14425c3989a95cfc0
ZeroaF77 <158405501+XzeroaF77@users.noreply.github.com>
2025-05-12 21:21:01 +0800

    Update ft_print_reverse_alphabet.c

--- beginner_level/ft_print_reverse_alphabet.c
+++ beginner_level/ft_print_reverse_alphabet.c
@@ -1,14 +1,14 @@
 //To print from z to a in program
 
-#include <unistd.h>
-
-void	ft_print_reverse_alphabet(char c)
+#include <unistd.h>//header file to write()
+//Function definition (also serves as declaration in C)
+void	ft_print_reverse_alphabet(char c)//Should take no argument since it generates 'z' to 'a' internally
 {
-	char	c;
-	c = 'z';
-	while (c >= 'a')
+	char	c;//declare a character variable to store current letter
+	c = 'z';//initialize c with 'z'
+	while (c >= 'a')//loop from 'z' to 'a'
 		{
-			write(1, &c, 1);
-			c--;
+			write(1, &c, 1);//write the current character to standard output
+			c--;//decrement to the previous character
 		}
 }
